b39590cb3491f98303814473a2b58b8fba53ceff
TorrieL <hmminicity2@163.com>
2019-09-04 23:15:17 +0800

    start from the very beginning-variables

---
+++ js-basics/variables.js
@@ -0,0 +1,8 @@
+var num = 1; //declare a variable, initialize it with a number
+num = 3;// and then update it with a string or any other datatype
+
+var price = 1.5//declare a decimal floating point
+var myName = 'Chimp';// declare a string
+var trueV = true;// declare a boolean
+var nullV = null;// declare a null-no value
+var und;// undefined means a variable that has been declared but not yet assigned a value
