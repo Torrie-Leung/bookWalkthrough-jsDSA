55517d77e37777246904224a73a6e97edca13ad4
TorrieL <hmminicity2@163.com>
2019-09-10 20:55:10 +0800

    arithmetic/asignment/comparison/logical operators

---
+++ js-basics/operators.js
@@ -0,0 +1,27 @@
+//arithmetic operator
+let num = 0;
+num = num+2;
+num = num*2;
+num = num/2;
+num++;
+num--;
+
+//assignment operator
+num+=1;
+num-=2;
+num*=3;
+num/=2;
+num%=3;
+console.log(num)
+
+//comparison operator
+console.log('num == 1: '+(num == 1))
+console.log('num === 1: '+(num === 1))
+console.log('num != 1: '+(num != 1))
+console.log('num >1: '+(num > 1))
+console.log('num <1: '+(num < 1))
+
+//logical operator
+console.log('true && false: '+(true && false));
+console.log('true || false: '+(true || false));
+console.log('!true: '+(!true));
