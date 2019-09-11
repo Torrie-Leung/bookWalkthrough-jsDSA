835681ba98a6086a621fb1db9845660781519d39
TorrieL <hmminicity2@163.com>
2019-09-12 00:34:07 +0800

    truthy & falsy

---
+++ js-basics/torf.js
@@ -0,0 +1,23 @@
+function testTruthy(val){
+  return val ? console.log(`${val} is`,'truthy'):console.log(`${val} is`,'falsy')
+}
+
+testTruthy(true);
+testTruthy(false);
+testTruthy(new Boolean(false)); // object is always true
+
+testTruthy('')
+testTruthy('Bender')
+testTruthy(new String(''));// object is always true
+
+testTruthy(1)
+testTruthy(-1)//true the result is false for +0, -0 or NaN
+testTruthy(NaN)
+testTruthy(new Number(NaN));// object is always true
+
+testTruthy({});// object is always true
+
+const T = {name: 'Callum'};
+testTruthy(T)
+testTruthy(T.name)
+testTruthy(T.monkey)
