7ccf0d72ecbd0dae4ba81c3b6ca049e6ca7b8ac3
TorrieL <hmminicity2@163.com>
2019-09-14 22:55:41 +0800

    conditional statement

---
+++ js-basics/conditional-statements.js
@@ -0,0 +1,25 @@
+let n = 0;
+//ternary operator
+(n ===1) ? n-- : n++
+
+let month = 9;
+if(month === 12) {
+  console.log('Dec')
+}else if(month == 9){
+  console.log('Sept')
+}else{
+  console.log('Moth is no Dec or Sept')
+}
+
+//switch
+let m = 9
+switch(m){
+  case 1:
+    console.log('Jan')
+    break
+  case 9:
+    console.log('Sept')
+    break
+  default:
+    console.log('Nov')
+}
