3c0f4a5862c1762166deff831e37e37badfb637c
TorrieL <hmminicity2@163.com>
2019-09-17 23:13:50 +0800

    fibonacci rookie practice

---
+++ js-basics/biliCourse/practice.js
@@ -0,0 +1,9 @@
+//grab the first 20 Fibonacci number
+let fibonacci = []
+fibonacci[0] = 1
+fibonacci[1] = 1
+
+for(let i = 2; i<20; i++){
+  fibonacci[i]=fibonacci[i-1]+fibonacci[i-2]
+}
+console.log(fibonacci)
